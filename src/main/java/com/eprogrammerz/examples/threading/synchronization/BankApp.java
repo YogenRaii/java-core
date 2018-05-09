68dec372ccea367ac8f10f0b50843280c35859b0
Yogen Rai <yogen.rai.992@gmail.com>
2018-05-09 15:50:45 -0700

    Updated thread naming while creation

--- src/main/java/com/eprogrammerz/examples/threading/synchronization/BankApp.java
+++ src/main/java/com/eprogrammerz/examples/threading/synchronization/BankApp.java
@@ -8,10 +8,8 @@ public class BankApp {
         final Transaction tx1 = new Transaction(customer);
         final Transaction tx2 = new Transaction(customer);
 
-        Thread t1 = new Thread(tx1);
-        Thread t2 = new Thread(tx2);
-        t1.setName("Tx-1");
-        t2.setName("Tx-2");
+        Thread t1 = new Thread(tx1, "Tx-1");
+        Thread t2 = new Thread(tx2, "Tx-2");
 
         t1.start();
         t2.start();
