eeebe07bf1fd6be5fd10a5ddef373565800143bb
Yogen Rai <yogen.rai.992@gmail.com>
2018-06-04 16:57:11 -0700

    Updated example around Optional

--- src/main/java/com/eprogrammerz/examples/java8/features/OptionalExample.java
+++ src/main/java/com/eprogrammerz/examples/java8/features/OptionalExample.java
@@ -1,9 +1,14 @@
 package com.eprogrammerz.examples.java8.features;
 
+import org.junit.Test;
+
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
+
+
 /**
- * Created by 542596 on 11/10/2016.
+ * Created by Yogen on 11/10/2016.
  *
  * Optional is a container object which is used to contain not-null objects.
  *
@@ -12,22 +17,19 @@ import java.util.Optional;
  * Has various utility to handle values as 'available' or 'not available' instead checking null values.
  */
 public class OptionalExample {
-    public static void main(String[] args) {
+    public Integer sum(Optional<Integer> num1,Optional<Integer> num2){
+        Integer val1 = num1.orElse(new Integer(0));
+        Integer val2 = num2.orElse(new Integer(0)); // num2.get();
+        return val1 + val2;
+    }
+
+    @Test
+    public void testSumWithOptionals() {
         //Optional.ofNullable - allows null to be a parameter
         Optional<Integer> num1 = Optional.ofNullable(null);
 
         Optional<Integer> num2 = Optional.of(new Integer(10));
 
-        System.out.println(OptionalExample.sum(num1,num2));
-    }
-
-    public static Integer sum(Optional<Integer> num1,Optional<Integer> num2){
-        //check if numbers have value or not
-        System.out.println("num1: "+num1.isPresent());
-        System.out.println("num2: "+num2.isPresent());
-
-        Integer val1 = num1.orElse(new Integer(0));
-        Integer val2 = num2.orElse(new Integer(0)); // num2.get();
-        return val1 + val2;
+        assertEquals(new Integer(10), sum(num1, num2));
     }
 }
