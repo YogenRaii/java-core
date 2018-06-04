f7caa15ee5647c29e95932f0ba7a5b65d44119f2
Yogen Rai <yogen.rai.992@gmail.com>
2018-06-04 15:23:24 -0700

    Added example for LocalDateTime conversion to Date

--- src/main/java/com/eprogrammerz/examples/java8/dateTime/DateTimeApiExample.java
+++ src/main/java/com/eprogrammerz/examples/java8/dateTime/DateTimeApiExample.java
@@ -1,6 +1,8 @@
 package com.eprogrammerz.examples.java8.dateTime;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 /**
  * Created by Yogen on 9/27/2017.
@@ -17,5 +19,9 @@ public class DateTimeApiExample {
 
         System.out.println(currentTime);
         System.out.println(changedTime);
+
+        Date threeDaysBack = Date.from(LocalDateTime.now().minusDays(3).atZone(ZoneId.systemDefault()).toInstant());
+
+        System.out.println(threeDaysBack);
     }
 }
