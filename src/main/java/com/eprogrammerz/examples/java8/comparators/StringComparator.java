c2de9b2c4a4347494121e647171e000767c0aa30
Yogen Rai <yogen.rai.992@gmail.com>
2018-06-06 19:42:53 -0700

    Added sort example for string

---
+++ src/main/java/com/eprogrammerz/examples/java8/comparators/StringComparator.java
@@ -0,0 +1,52 @@
+package com.eprogrammerz.examples.java8.comparators;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Yogen Rai
+ */
+public class StringComparator {
+    public void sortNewWay(final List<String> words) {
+        words.sort(Comparator.comparing(String::length)
+                .thenComparing(Comparator.reverseOrder()).reversed());
+    }
+
+    public void sortOldWay(final List<String> words) {
+        words.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s2.length() == s1.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s2.length() - s1.length();
+                }
+            }
+        });
+    }
+
+    @Test
+    public void testSortOldWay() {
+        final List<String> names = Arrays.asList("Yogen", "Dipen", "Rita", "Dipa", "Prateema");
+        final List<String> sorted = Arrays.asList("Prateema", "Dipen", "Yogen", "Dipa", "Rita");
+        // execute
+        sortOldWay(names);
+        // verify
+        assertEquals(sorted, names);
+    }
+
+    @Test
+    public void testSortNewWay() {
+        final List<String> names = Arrays.asList("Yogen", "Dipen", "Rita", "Dipa", "Prateema");
+        final List<String> sorted = Arrays.asList("Prateema", "Dipen", "Yogen", "Dipa", "Rita");
+        // execute
+        sortNewWay(names);
+        // verify
+        assertEquals(sorted, names);
+    }
+}
