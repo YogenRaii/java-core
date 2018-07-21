5b778cc999b128d6a017da51dcc61e03274fa318
Yogen Rai <yogen.rai.992@gmail.com>
2018-07-21 14:24:59 -0700

    Added example for bit manipulation

---
+++ src/main/java/com/eprogrammerz/examples/algorithm/general/LonelyInteger.java
@@ -0,0 +1,28 @@
+package com.eprogrammerz.examples.algorithm.general;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Yogen Rai
+ *
+ * Consider an array of integers where all but one of the integers occur in pairs.
+ * In other words, every element occurs exactly twice except for one unique element. Find the unique element.
+ * For example, given the array [1,1,2,3,2], you would return 3.
+ */
+public class LonelyInteger {
+    static int findLonely(List<Integer> arr) {
+
+        int num = 0;
+
+        for (int n: arr) {
+            num ^= n;  // 0 ^ 1 = 1
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLonely(Arrays.asList(1,1,2)));
+    }
+}
