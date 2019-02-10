8792c8c3430d4063854373fe8412a4adbd59cf84
Yogen Rai <yogen.rai.992@gmail.com>
2019-02-10 11:41:25 -0800

    Added example to find the longest palindrome with better time complexity

--- src/main/java/com/eprogrammerz/examples/algorithm/general/LongestPalindrome.java
+++ src/main/java/com/eprogrammerz/examples/algorithm/general/LongestPalindrome.java
@@ -10,9 +10,15 @@ import static junit.framework.TestCase.assertTrue;
  *
  * banana -> anana
  * racecar -> racecar
- *
+ * ac -> a
+ * bb -> bb
  */
 public class LongestPalindrome {
+    /**
+     * O(n^3)
+     * @param str
+     * @return longest palindrome substring
+     */
     public String findLongestPalindrome(String str) {
         String result = "";
 
@@ -22,12 +28,15 @@ public class LongestPalindrome {
             StringBuilder substr = new StringBuilder();
             substr.append(str.charAt(start));
 
-            for (int end = start + 1; end < str.length(); end++) {
-                substr.append(str.charAt(end));
+            for (int end = start + 1; end <= str.length(); end++) {
                 String possiblePalindrome = substr.toString();
 
                 if (isPalindrome(possiblePalindrome) && possiblePalindrome.length() > result.length()) {
                     result = possiblePalindrome;
+                }
+
+                if (end < str.length()) {
+                    substr.append(str.charAt(end));
                 }
             }
         }
@@ -44,7 +53,66 @@ public class LongestPalindrome {
     public void testFindLongestPalindrome() {
         assertTrue(findLongestPalindrome("").isEmpty());
         assertTrue(findLongestPalindrome(null).isEmpty());
+        assertEquals("a", findLongestPalindrome("a"));
+        assertEquals("bb", findLongestPalindrome("bb"));
         assertEquals("anana", findLongestPalindrome("banana"));
         assertEquals("racecar", findLongestPalindrome("racecar"));
+    }
+
+    /**
+     * O(n^2)
+     *
+     * This approach considers each char as mid and find the possible palindrome out of it
+     * This has to be taken care for both odd and even centric chars
+     *
+     * @param str
+     * @return LPS
+     */
+    public String findLongestPalindromeBetter(String str) {
+        if (str == null || str.isEmpty()) return "";
+        if (str.length() == 1) return str;
+
+        int start = 0;
+        int len = 1;
+
+        for (int idx = 1; idx < str.length(); idx++) {
+            // considering odd center
+            int low = idx - 1;
+            int high = idx;
+
+            while (low >= 0 && high < str.length() && str.charAt(low) ==  str.charAt(high)) {
+                if (high - low + 1 > len) {
+                    start = low;
+                    len = high - low + 1;
+                }
+                low--;
+                high++;
+            }
+
+            // considering even at center
+            low = idx - 1;
+            high = idx + 1;
+            while (low >= 0 && high < str.length() && str.charAt(low) ==  str.charAt(high)) {
+                if (high - low + 1 > len) {
+                    start = low;
+                    len = high - low + 1;
+                }
+                low--;
+                high++;
+            }
+        }
+
+        return str.substring(start, start + len);
+    }
+
+    @Test
+    public void testFindLongestPalindromeBetter() {
+        assertTrue(findLongestPalindromeBetter("").isEmpty());
+        assertTrue(findLongestPalindromeBetter(null).isEmpty());
+        assertEquals("a", findLongestPalindromeBetter("a"));
+        assertEquals("a", findLongestPalindromeBetter("ac"));
+        assertEquals("bb", findLongestPalindromeBetter("bb"));
+        assertEquals("anana", findLongestPalindromeBetter("banana"));
+        assertEquals("racecar", findLongestPalindromeBetter("racecar"));
     }
 }
