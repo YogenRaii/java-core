c406dbfa92424ce588583c447a2ca6b1e413345d
Yogen Rai <yogen.rai.992@gmail.com>
2019-02-16 08:56:19 -0800

    Updated powerset to find subsets in ascending order

--- src/main/java/com/eprogrammerz/examples/algorithm/crackingCoding/PowerSet.java
+++ src/main/java/com/eprogrammerz/examples/algorithm/crackingCoding/PowerSet.java
@@ -1,5 +1,7 @@
 package com.eprogrammerz.examples.algorithm.crackingCoding;
 
+import org.junit.Test;
+
 import java.util.*;
 
 /**
@@ -44,9 +46,47 @@ public class PowerSet {
         return powerSet;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testPowerset() {
         Set<Set<Integer>> powerSet = findPowerSet(Arrays.asList(1,2,3,4));
         System.out.println(powerSet.size());
         System.out.println(powerSet);
+    }
+
+    /**
+     * https://www.interviewbit.com/problems/subset/
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(List<Integer> nums) {
+        // P(n) = P(n-1) + {P(n-1) + an}
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());
+
+        if (nums.isEmpty()) return subsets;
+
+        // sort to make sure elements are in ascending order
+        Collections.sort(nums);
+        subsets(nums, subsets, new ArrayList<>(), 0);
+        return subsets;
+    }
+
+    public void subsets(List<Integer> nums, List<List<Integer>> result, List<Integer> temp, int index) {
+        for (int i = index; i < nums.size(); i++) {
+            temp.add(nums.get(i));
+            result.add(new ArrayList<>(temp));
+            subsets(nums, result, temp, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    @Test
+    public void testSubsets() {
+        List<List<Integer>> powerSet = subsets(Arrays.asList(1,2,3,4));
+        System.out.println(powerSet.size());
+        System.out.println(powerSet);
+
+        System.out.println(subsets(Arrays.asList( 15, 20, 12, 19, 4)));
     }
 }
