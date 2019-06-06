5ab3f3bf7b1d8c4b6515e2714d717b9caf578cf2
Yogen Rai <yogen.rai.992@gmail.com>
2019-06-05 21:44:21 -0500

    Updated example to check balanced BST

--- src/main/java/com/eprogrammerz/examples/algorithm/trees/BalancedBinarySearchTree.java
+++ src/main/java/com/eprogrammerz/examples/algorithm/trees/BalancedBinarySearchTree.java
@@ -6,6 +6,12 @@ import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class BalancedBinarySearchTree {
+    /**
+     * O(n logn) since we calculate height of tree on each node starting from root node
+     *
+     * @param tree
+     * @return
+     */
     public boolean isBalancedBST(Node tree) {
         if (tree == null) return true;
         int heightDiff = Math.abs(height(tree.left) - height(tree.right));
@@ -54,5 +60,68 @@ public class BalancedBinarySearchTree {
          */
         root.right.right.right = new Node(17);
         assertFalse(isBalancedBST(root));
+    }
+
+    /**
+     * Return false as soon as we detect tree is not balanced at certain node
+     *
+     * O(n) - time
+     * O(h) - height
+     * @param tree
+     * @return
+     */
+    public boolean isBalancedBSTBetter(Node tree) {
+        return checkHeight(tree) != Integer.MIN_VALUE;
+    }
+
+    private int checkHeight(Node tree) {
+        if (tree == null) return 0;
+
+        int left = checkHeight(tree.left);
+        if (left == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        int right = checkHeight(tree.right);
+        if (right == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        if (Math.abs(left -right) > 1) {
+            return Integer.MIN_VALUE;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    @Test
+    public void testBalancedBinarySearchTreeBetter() {
+
+        /**
+         *          10
+         *       /     \
+         *     7         12
+         *   /   \        \
+         *  4     8        15
+         *
+         */
+
+        Node root = new Node(10);
+        root.left = new Node(7);
+        root.right = new Node(12);
+        root.left.left = new Node(4);
+        root.left.right = new Node(8);
+        root.right.right = new Node(15);
+
+        assertTrue(isBalancedBSTBetter(root));
+
+
+        /**
+         *          10
+         *       /     \
+         *     7         12
+         *   /   \        \
+         *  4     8        15
+         *                  \
+         *                   17
+         *
+         */
+        root.right.right.right = new Node(17);
+        assertFalse(isBalancedBSTBetter(root));
     }
 }
