b2ad871794cf0f5b6705af471c2a5de7ffa36a5c
Yogen Rai <yogen.rai.992@gmail.com>
2019-11-03 19:24:47 -0600

    Updated solution for checking palindrome

--- src/main/java/com/eprogrammerz/examples/algorithm/leetcode/Palindrome.java
+++ src/main/java/com/eprogrammerz/examples/algorithm/leetcode/Palindrome.java
@@ -18,7 +18,7 @@ public class Palindrome {
         assertFalse(isPalindrome(122321));
     }
 
-    public static boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x) {
         int originalVal = x;
         int reversedX = 0;
         while(x != 0) {
@@ -26,5 +26,47 @@ public class Palindrome {
             x /= 10;
         }
         return originalVal == reversedX;
+    }
+
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) return true;
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start <= end) {
+            char startChar = s.charAt(start);
+            char endChar = s.charAt(end);
+
+            // startChar is lowercase
+            if (startChar >= 'a' && startChar <= 'z') {
+                startChar = (char)('A' + (startChar - 'a'));
+            }
+
+            if (endChar >= 'a' && endChar <= 'z') {
+                endChar = (char)('A' + (endChar - 'a'));
+            }
+
+            if ((startChar < 'A' || startChar > 'Z') && (startChar < '0' || startChar > '9')) {
+                start++;
+                continue;
+            }
+
+            if ((endChar < 'A' || endChar > 'Z') && (endChar < '0' || endChar > '9')) {
+                end--;
+                continue;
+            }
+
+            if (startChar != endChar) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    @Test
+    public void test() {
+        assertTrue(isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
