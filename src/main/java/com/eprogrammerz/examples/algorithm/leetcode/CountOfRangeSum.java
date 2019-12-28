4c3173c4f1a69c07375ed826920920c9e3e4157d
Yogen Rai <yogen.rai.992@gmail.com>
2019-12-27 19:04:19 -0600

    Updated count range sum with merge sort

--- src/main/java/com/eprogrammerz/examples/algorithm/leetcode/CountOfRangeSum.java
+++ src/main/java/com/eprogrammerz/examples/algorithm/leetcode/CountOfRangeSum.java
@@ -2,8 +2,6 @@ package com.eprogrammerz.examples.algorithm.leetcode;
 
 import org.junit.Test;
 
-import java.util.TreeMap;
-
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -12,24 +10,55 @@ import static org.junit.Assert.assertEquals;
 public class CountOfRangeSum {
     public int countRangeSum(int[] nums, int lower, int upper) {
 
-        TreeMap<Long, Integer> map = new TreeMap<>();
-        map.put(0L, 1);
-
-        long sum = 0;
-
-        int count = 0;
-
-        for (int n : nums) {
-            sum += n;
-
-            for (int cnt : map.subMap(sum - upper, true, sum - lower, true).values()) {
-                count += cnt;
-            }
-
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        long[] sum = new long[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
         }
 
+        return mergeSort(sum, 0, sum.length - 1, lower, upper);
+    }
+
+    private int mergeSort(long[] arr, int lo, int hi, int lower, int upper) {
+        if (lo >= hi) return 0;
+        int mid = lo + (hi - lo) / 2;
+
+        int count = mergeSort(arr, lo, mid, lower, upper) + mergeSort(arr, mid + 1, hi, lower, upper);
+
+        for (int i = lo, j = mid + 1, k = mid + 1; i <= mid; i++) {
+            while (j <= hi && arr[j] - arr[i] < lower) j++;
+            while (k <= hi && arr[k] - arr[i] <= upper) k++;
+
+            count += (k - j);
+        }
+
+        merge(arr, lo, mid, hi);
+
         return count;
+    }
+
+    private void merge(long[] arr, int lo, int mid, int hi) {
+        long[] copy = new long[hi - lo + 1];
+
+        int p = lo;
+        int q = mid + 1;
+        int i = 0;
+        while (p <= mid && q <= hi) {
+            if (arr[p] < arr[q]) {
+                copy[i++] = arr[p++];
+            } else {
+                copy[i++] = arr[q++];
+            }
+        }
+
+        while (p <= mid) {
+            copy[i++] = arr[p++];
+        }
+
+        while (q <= hi) {
+            copy[i++] = arr[q++];
+        }
+
+        System.arraycopy(copy, 0, arr, lo, hi - lo + 1);
     }
 
     @Test
